7e632203463420ff29e461c57d968fdb93910634
srabonmozumder2 <srabonmozumder29@gmail.com>
2023-10-31 12:21:30 +0600

    all init

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+        $('.site-navigation').addClass("active")
+    } else {
+        $('.site-navigation').removeClass("active")
+    };
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
